2a9a37f819e6654fd5f77eabae230a9b39f9acca
stan <stnwa3@morgan.edu>
2021-09-24 17:48:58 -0400

    Flixter part 2 assignment

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,9 @@
 package com.example.flixster.adapters;
 
+import static android.app.ActivityOptions.makeSceneTransitionAnimation;
+
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,15 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.flixster.MainActivity;
+import com.example.flixster.MovieDetailsActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -62,21 +73,25 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        ConstraintLayout container;
+        YouTubePlayerView youTubePlayerView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+            youTubePlayerView = itemView.findViewById(R.id.player);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
-            boolean isLandsape = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+            boolean isLandscape = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
             // If phone is in landscape
-            if (isLandsape){
+            if (isLandscape){
                 // then imageUrl = back drop image
                 imageUrl = movie.getBackdropPath();
             } else {
@@ -84,13 +99,26 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             }
             // Glide.with(context).load(imageUrl).into(ivPoster);
             //load image using glide
-            int placeholderId = isLandsape ? R.drawable.flicks_backdrop_placeholder : R.drawable.flicks_movie_placeholder2;
+            int placeholderId = isLandscape ? R.drawable.flicks_backdrop_placeholder : R.drawable.flicks_movie_placeholder2;
             Glide.with(context)
                     .load(imageUrl)
                     .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
                     .placeholder(placeholderId)
                     .error(placeholderId)
                     .into(ivPoster);
+
+            // 1. register the click listener on the whole row (container)
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // 2. navigate to a new activity on tap
+                    Intent intent = new Intent(context, MovieDetailsActivity.class);
+                    intent.putExtra("movie", Parcels.wrap(movie));
+//                    ActivityOptionsCompat options = ActivityOptionsCompat.
+//                            makeSceneTransitionAnimation(MainActivity.this, (View)youTubePlayerView, "profile");
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 }
